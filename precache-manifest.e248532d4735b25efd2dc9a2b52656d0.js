7631840b342089f50b8432708fedb6acafd882b4
Tissa Khosla <tissa.music@gmail.com>
2019-12-26 15:28:02 -0500

    Updates

---
+++ precache-manifest.e248532d4735b25efd2dc9a2b52656d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a1b0d5751e8a52d4849d54ddd75f53",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af789059fd6061254b8a",
+    "url": "/portfolio/static/css/main.9113e8e8.chunk.css"
+  },
+  {
+    "revision": "14c415a9462b4ea8018f",
+    "url": "/portfolio/static/js/2.ccd056e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ccd056e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af789059fd6061254b8a",
+    "url": "/portfolio/static/js/main.96cd9a90.chunk.js"
+  },
+  {
+    "revision": "1daf2e50e9a968157302",
+    "url": "/portfolio/static/js/runtime-main.5e97d231.js"
+  },
+  {
+    "revision": "d5ca4fba0886c4f827acb4c42c765e1e",
+    "url": "/portfolio/static/media/atTheComputer.d5ca4fba.png"
+  },
+  {
+    "revision": "a51cc3f33c697e3ea8089bea24c678e3",
+    "url": "/portfolio/static/media/profilePicture.a51cc3f3.png"
+  },
+  {
+    "revision": "2b2207b983a70cf0b10d078a7609509d",
+    "url": "/portfolio/static/media/projectsScreenshot.2b2207b9.JPG"
+  }
+]);
